73ff7f073b96dfeb21a668906ebde16a5d2f3558
Emir Balcı <aerangalerind@hotmail.com>
2022-10-08 09:49:23 +0300

    added arrays

---
+++ 7 - Arrays/Solution.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+        printReverse(arr);
+        bufferedReader.close();
+    }
+
+    public static void printReverse(List<Integer> arr) {
+        for (int i = arr.size() - 1; i >= 0; i--)
+            System.out.print(arr.get(i) + " ");
+    }
+}
